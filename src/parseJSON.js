717c6e36db2ff2a8ef7a6a5b59ee9ed4f8a794e2
Hack Reactor Student <student@hackreactor.com>
2017-10-09 16:41:22 -0700

    Attempt to solve parseJSON (woohoo!)

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,69 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // '{ key: [2] }'
+  
+  let char = json[0];    
+
+  // if identified
+  if (char === "{") {
+    // is object
+    let pairCounter = 0;
+    let endOfThis; // end character of the object
+
+    for (let i = 1; i < json.length; i++) {  
+      const thisChar = json[i];
+      
+      if (thisChar === "{") {
+        pairCounter++;
+      }
+
+      if (thisChar === "}") {
+        if (pairCounter > 0) {
+          pairCounter--;
+        } else {
+          endOfThis = i;   
+        }
+      }
+    }
+
+    const thisObj = json.slice(1, endOfThis);
+
+    let commaPlaces = [];
+
+    for (let i = 0; i < thisObj.length; i++) {
+      let thisChar = thisObj[i];
+
+      if (thisChar === ",") {
+        commaPlaces.push(i);
+      }      
+  
+    }
+
+    const kvCount = commaPlaces.length + 1;
+
+    let colonPlace = [];
+
+    for(let i = 0; i < kvCount; i++) {
+      // search for a colon
+      // let startChar = 
+      for(let j = commaPlaces[i]; j < commaPlaces[i + 1]; j++) {
+      
+      }
+    }
+// { 'one' : 4, 'two' : { 'yes':8, 'no':10 }, 'three': 6 }
+
+// thisObj[1,commaPlaces[0]]
+// thisObj[commaPlaces[0]+1,commaPlaces[1]-1]
+
+    // join the parseJSON(thisObj) with  and empty object'
+    let result = {};
+
+    console.log(result);
+
+    return result;
+  } // end of object case
+
+  
+
 };
